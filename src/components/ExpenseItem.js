90abaec0682c62a9262366c4ff4b09c496f88e69
arpitbhardwaj <arpitbhardwaj09@gmail.com>
2022-10-26 11:07:59 +0530

    Another component and data funneling

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,14 +1,18 @@
 import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
 
 function ExpenseItem(props) {
+  // const expenseDate = new Date(2022, 10, 26);
+  // const expenseTitle = "Home Insurance";
+  // const expenseAmount = 12500;
 
-    // const expenseDate = new Date(2022, 10, 26);
-    // const expenseTitle = "Home Insurance";
-    // const expenseAmount = 12500;
+  const month = props.date.toLocaleString("en-US", { month: "long" });
+  const day = props.date.toLocaleString("en-US", { month: "2-digit" });
+  const year = props.date.getFullYear();
 
   return (
     <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
+      <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">{props.amount}</div>
