176fc505e955170c818b468cd991a9db2e32570d
arpitbhardwaj <arpitbhardwaj09@gmail.com>
2022-10-27 13:57:17 +0530

    Added ExpenseFilter

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -12,22 +12,22 @@ function ExpenseItem(props) {
   const day = props.date.toLocaleString("en-US", { month: "2-digit" });
   const year = props.date.getFullYear(); */
 
-  const [title, setTitle] = useState(props.title);
+  //const [title, setTitle] = useState(props.title);
 
-  const clickHandler = () => {
+  /* const clickHandler = () => {
     //title = "Updated!";
     setTitle("Updated!");
     console.log(title);
-  }
+  } */
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">{props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
+      {/* <button onClick={clickHandler}>Change Title</button> */}
     </Card>
   );
 }
