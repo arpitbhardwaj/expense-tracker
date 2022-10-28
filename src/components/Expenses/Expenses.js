eb3a84fce69fe86fdcab73bc3989190433f001df
arpitbhardwaj <arpitbhardwaj09@gmail.com>
2022-10-28 09:51:48 +0530

    Added Chart feature

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,9 +1,9 @@
-import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import "./Expenses.css";
 import ExpensesFilter from "./ExpensesFilter";
 import { useState } from "react";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 
 function Expenses(props) {
   const [filteredYear, setFilteredYear] = useState("2020");
@@ -58,7 +58,7 @@ function Expenses(props) {
             date={expense.date}
           />
         ))} */}
-
+        <ExpensesChart expenses={filteredExpenses} />
         <ExpensesList items={filteredExpenses} />
     </Card>
   );
